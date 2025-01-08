ac9ddcbec8222f039fbf0c92b7082cff03a945f1
Aadityavv <aadityavv9@gmail.com>
2025-01-08 21:58:38 +0530

    Modules updated

--- app/api/settings/security/route.js
+++ app/api/settings/security/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { connectToDatabase } from "@/lib/db";
-import bcrypt from "bcryptjs";
+// import bcrypt from "bcryptjs";
 
 export async function PUT(req) {
   const { db } = await connectToDatabase();
